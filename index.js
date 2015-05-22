a733df62a7b553ca4182e5c0ff616355d3851024
wanderanimrod <wanderanimrod@gmail.com>
2015-05-22 20:59:32 +0300

    writes post data to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json());
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/node_messages');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+app.post('/', function(request, response) {
+	var db = request.db;
+	var collection = db.get('node_messages');
+	request.body.forEach(function(node_message) {
+		collection.insert(request.body);
+	});
+  	console.log('node messages added');
+  	response.send('node messages added');
 });
 
 app.listen(app.get('port'), function() {
